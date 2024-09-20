bb651e691df4e625f3176140cbb6202fff43495a
aryyyn <aryan.211618@ncit.edu.np>
2024-09-20 11:33:15 +0545

    Initial setup for React and Django

---
+++ Frontend/Flowify/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+
+          Start Editing from <code>src/App.jsx</code> 
+
+    </>
+  )
+}
+
+export default App
